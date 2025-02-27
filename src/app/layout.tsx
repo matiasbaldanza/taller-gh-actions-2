0cd50953ae76ecad5e12381547abaf58814cdce4
Matias Baldanza <matiasbaldanza@gmail.com>
2025-02-27 15:24:38 -0300

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="flex flex-col items-center justify-center h-screen gap-2">
+          {children}
+        </main>
       </body>
     </html>
   );
