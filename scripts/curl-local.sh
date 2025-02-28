82f17caefb0bbfbdc21887a53bbb6248139047ce
Matias Baldanza <matiasbaldanza@gmail.com>
2025-02-28 14:29:15 -0300

    update curl-local.sh to remove jq (not installed on server)

--- scripts/curl-local.sh
+++ scripts/curl-local.sh
@@ -1,13 +1,13 @@
 # Testear ruta API /api/routelocalmente
 echo "🔍 Testeando ruta API localmente..."
-curl -X GET http://localhost:8080/api/route | jq
+curl -X GET http://localhost:8080/api/route
 
 # Testear / localmente
 echo "🔍 Testeando / localmente..."
-curl -X GET http://localhost:8080/ | jq
+curl -X GET http://localhost:8080/
 
 # Testear /hello localmente
 echo "🔍 Testeando /hola localmente..."
-curl -X GET http://localhost:8080/hola | jq
+curl -X GET http://localhost:8080/hola
 
 
